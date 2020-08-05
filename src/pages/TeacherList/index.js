191a8856668e9b5bd7d94f4dd6cfb6894cc9ec3d
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-04 22:26:23 -0300

    Extracts teacher card to component

--- src/pages/TeacherList/index.js
+++ src/pages/TeacherList/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import PageHeader from 'components/PageHeader'
-import whatsappIcon from 'assets/images/icons/whatsapp.svg'
+import TeacherItem from 'components/TeacherItem'
 
 import { Container, Form, Main } from './styles'
 
@@ -26,34 +26,9 @@ const TeacherList = () => {
       </PageHeader>
 
       <Main>
-        <article>
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/4058722?s=460&u=aa7ccd0559a5e7265c0c29dc5e98e3503cfc0962&v=4" alt="Marina Ferreia" />
-
-            <div>
-              <strong>Marina Ferreira</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            <br /><br />
-            Iste eum nesciunt excepturi, alias saepe incidunt mollitia aperiam voluptates optio inventore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem distinctio tempore, vitae temporibus similique repellendus quam asperiores totam accusantium possimus?
-          </p>
-
-          <footer>
-            <p>
-              Hour rate
-              <strong>R$ 20,00</strong>
-            </p>
-
-            <button type="type">
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </Main>
     </Container>
   )
