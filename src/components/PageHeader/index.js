343ed9ff02db493cf985e246dd68827c74ea7f50
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-07 19:14:43 -0300

    Page header styled components

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom'
 import logo from 'assets/images/logo.svg'
 import backIcon from 'assets/images/icons/back.svg'
 
-import { Header } from './styles'
+import { Header, Topbar, Content } from './styles'
 
 const PageHeader = ({ title, description, children }) => {
   return (
     <Header>
-      <div>
+      <Topbar>
         <Link to="/">
           <img src={backIcon} alt="Back" />
         </Link>
         <img src={logo} alt="Proffy" />
-      </div>
+      </Topbar>
 
-      <section>
+      <Content>
         <strong>{title}</strong>
         {description && <p>{description}</p>}
         {children}
-      </section>
+      </Content>
     </Header>
   )
 }
--- src/components/PageHeader/styles.js
+++ src/components/PageHeader/styles.js
@@ -5,71 +5,69 @@ export const Header = styled.header`
   flex-direction: column;
   background: var(--color-primary);
 
-  > div {
-    width: 90%;
-    margin: 0 auto;
-    padding: 1.6rem 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: var(--color-text-in-primary);
+  @media (min-width: 700px) {
+    height: 340px;
+  }
+`
+export const Topbar = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  padding: 1.6rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--color-text-in-primary);
 
-    a {
-      height: 3.2rem;
-      opacity: 1;
-      transition: opacity .2s;
+  a {
+    height: 3.2rem;
+    opacity: 1;
+    transition: opacity .2s;
 
-      &:hover {
-        opacity: .6
-      }
-    }
-
-    > img {
-      height: 1.6rem;
+    &:hover {
+      opacity: .6
     }
   }
 
-  > section {
-    width: 90%;
-    margin: 0 auto;
-    position: relative;
-    margin: 3.2rem auto;
-
-    strong {
-      font: 700 3.6rem Archivo;
-      line-height: 4.2rem;
-      color: var(--color-title-in-primary)
-    }
-
-    p {
-      max-width: 30rem;
-      font-size: 1.8rem;
-      line-height: 2.6rem;
-      color: var(--color-text-in-primary);
-      margin-top: 2.4rem;
-    }
+  > img {
+    height: 1.6rem;
   }
 
   @media (min-width: 700px) {
-    height: 340px;
+    max-width: 1100px;
+  }
+`
+export const Content = styled.section`
+  width: 90%;
+  margin: 0 auto;
+  position: relative;
+  margin: 3.2rem auto;
 
-    > div {
-      max-width: 1100px;
-    }
+  strong {
+    font: 700 3.6rem Archivo;
+    line-height: 4.2rem;
+    color: var(--color-title-in-primary)
+  }
 
-    > section {
-      flex: 1;
-      max-width: 740px;
-      margin: 0 auto;
-      padding-bottom: 48px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
+  p {
+    max-width: 30rem;
+    font-size: 1.8rem;
+    line-height: 2.6rem;
+    color: var(--color-text-in-primary);
+    margin-top: 2.4rem;
+  }
 
-      strong {
-        max-width: 350px;
-      }
+  @media (min-width: 700px) {
+    flex: 1;
+    max-width: 740px;
+    margin: 0 auto;
+    padding-bottom: 48px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    strong {
+      max-width: 350px;
     }
   }
 `
